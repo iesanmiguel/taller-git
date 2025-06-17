81673f5b7ec1216f792f77ad9a056645491f94fe
iesanmiguel <iesanmiguel.aceleracion2024@gylgroup.com>
2025-06-17 10:59:08 -0300

    seed

---
+++ src/test/java/gylgroup/tallergit/TallerGitApplicationTests.java
@@ -0,0 +1,13 @@
+package gylgroup.tallergit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
